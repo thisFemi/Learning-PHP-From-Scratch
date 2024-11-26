ec5e2cad0f2f24a60e97be613f0ae9b4ad1a293c
Oluwafemi <oluwafemijacob1@gmail.com>
2024-11-26 17:01:39 +0100

    dynamic and enviroment config

--- functions.php
+++ functions.php
@@ -2,3 +2,7 @@
 function urlIs($value){
     return $_SERVER['REQUEST_URI']==$value;
 }
+
+function dd($data){
+    return var_dump($data);
+}
--- index.php
+++ index.php
@@ -1,3 +1,35 @@
 <?php
 require "functions.php";
-require "router.php";
+require "Database.php";
+
+//require "router.php";
+
+
+
+//connect to MYSQL database.
+// class Person{
+//     public $name;public $age;
+
+//     public function breath(){
+//     echo $this->name . "is breathing";
+//     }
+// }
+
+// $person=new Person();
+// $person->name="Femi ";
+// $person->age=25;
+// $person->breath();
+//Connnecting to db, and execute a query.
+
+$config=require ("config.php");
+
+$db=new Database($config['database']);
+
+$posts=$db->query("select * from posts")->fetch();
+;
+
+
+dd($posts);
+// foreach($posts as $post){
+//     echo "<li>" . $post['title'] ."</li>";
+// }
